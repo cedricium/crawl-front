c27de8031208e57a4a78763d415b4bd17bd4d7b8
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2019-07-20 22:56:37 -0500

    webpack optimizations + babel + prettierrc + eslint
    boilerplate configuration files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,46 +1,85 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const path = require( 'path' );
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
+module.exports = (env) => {
+  // for environment variables
+  const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return {
     context: __dirname,
-    entry: './src/index.js',
+    mode: 'production',
+    entry: {
+      index: './src/index.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.css']
+    },
     output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js',
-        publicPath: '/',
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      publicPath: '/'
     },
     devServer: {
-        historyApiFallback: true
-     },
-    module: { 
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_module/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css?$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(png|j?g|svg|gif)?$/,
-                use: 'file-loader'
-            },
-            { 
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                loader: "url-loader?limit=10000&mimetype=application/font-woff" 
-              },
-              { 
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                loader: "file-loader" 
-              }
-        ]
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_module/,
+          use: ['babel-loader', 'eslint-loader']
+        },
+        {
+          test: /\.css?$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg|gif|jpg|jpeg|bmp)(\?.*$|$)/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          loader: 'url-loader'
+        }
+      ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve( __dirname, 'public/index.html' ),
-            filename: 'index.html'
-        })
-    ]
+      new CleanWebpackPlugin(),
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+        title: 'Caching'
+      }),
+      new webpack.DefinePlugin(envKeys)
+
+    ],
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      minimizer: [new UglifyJsPlugin()],
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true
+          },
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+            maxSize: 200000
+          }
+        }
+      }
+    }
+  };
 };
