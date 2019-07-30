7def32913e2f6929e6d494dc5473871acd06072f
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2019-07-29 23:02:42 -0500

    fetch servers from API

--- frontend/src/components/ServerCardGrid.js
+++ frontend/src/components/ServerCardGrid.js
@@ -1,35 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import ServerCard from './ServerCard';
-import Fire from '../config/Fire';
 import LoadingIndicator from './Loading';
 import '../styles/ServerCardGrid.css';
 
-const Centered = styled.div`
-  width: ${window.innerWidth};
-  margin: 0 auto;
-  padding-top: 5vw;
+
+const Centered = styled.div `
+    width:        ${window.innerWidth};
+    margin:       0 auto;
+    padding-top:  5vw;
 `;
 
-function ServerCardGrid (props) {
-  const [serverList, setServerList] = useState([]);
-  const [hasPulledServers, setPulled] = useState(false);
-  const [serverCount, setServerCount] = useState(0);
 
-  useEffect(() => {
-    console.log('useEffect() called');
-    setPulled(false);
-    Fire.database()
-      .ref('servers')
-      .on('value', snapshot => {
-        const snapVal = snapshot.val();
-        setServerCount(Object.keys(snapVal).length);
-        setServerList(snapVal);
+const ServerCardGrid = ( ) => {
+
+  const [ serverList,       setServerList   ] = useState(   [ ]  );
+  const [ hasPulledServers, setPulled       ] = useState(  false );
+  const [ serverCount,      setServerCount  ] = useState(    0   );  
+  const [ newServers,       setNewServers   ] = useState(  false );
+  
+  useEffect( ( ) => {
+
+    if ( !hasPulledServers ) {
+
+      ( async ( ) => {
+
+        let res = await fetch ("http://localhost:8000/api/Discord_Server");
+
+        let response = await res.json ( );
+
+        // update states
+        setServerList (response);
+        console.log   (response)
+
+        let ct=0, last_id = 0;
+
+        let dorah = response.map ( function( server ) {
+          ct += 1;
+          last_id = server.id;
+          console.log('last ID: ' + server.id);
+        });
+
+        setServerCount(ct);
         setPulled(true);
-      });
-  }, [serverCount]);
-  // ^^ passing the count makes sure React only calls
-  // this when the count changes
+
+        console.log('Count: ' + ct)
+      })();
+    }
+  }, []);
+    
 
   function renderLoadingIndicator () {
     if (!hasPulledServers) {
@@ -43,27 +62,48 @@ function ServerCardGrid (props) {
     }
   }
 
+
   function renderGrid (page) {
     var paginateServers = [];
     if (serverList && serverCount > 0) {
       var count = 0;
-      Object.keys(serverList).map((servers) => {
-        if (servers && serverList[servers] && serverList[servers]['guild']) {
+      serverList.map((server) => {
+        console.log('rendering server ' + server['invite_link']);
+        if (server) {
           count++; // we use the count to control animation delay
-          if (count <= 20) {
+          if (count <= 20000) {
             paginateServers.push(<li key={count}>
               <ServerCard
                 cardNumber={count}
                 key={count}
-                code={serverList[servers]['code']}
-                data={serverList[servers]}
+                code={server['invite_link']}
+                data={server}
               /></li>);
           }
         }
       });
-      return (<ul>{ paginateServers }</ul>);
+      return (
+        <div>
+          <ul>
+            <li>
+              This is a new server
+            </li>
+            <li>
+              This is a new server
+            </li>
+            <li>
+              This is a new server
+            </li>
+          </ul>
+          <ul>
+            { paginateServers.reverse() }
+          </ul>
+
+        </div>
+      );
     }
   }
+
 
   return (
     <div>
@@ -73,6 +113,7 @@ function ServerCardGrid (props) {
       </div>
     </div>
   );
-}
+};
+
 
 export default ServerCardGrid;
