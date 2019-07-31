d8488b5860b38fd21c9bf15feec58d7d7a370b0d
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2019-07-31 15:30:41 -0500

    fixed navbar rendering

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,72 +1,46 @@
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
-import styled from 'styled-components';
 import React, { Component } from 'react';
-import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
-console.log(_.join(['Another', 'module', 'loaded!'], ' '));
+import '../styles/Navbar.css';
 
-// Add spacing in between navbar and content
-const ShiftDown = styled.div`
-  margin-bottom: 10vw;
-`;
-
-class Navbar extends Component {
+class Header extends Component {
   constructor () {
     super();
     this.dropdown = this.dropdown.bind(this);
     this.state = {
-      menuClass: 'header-menu',
-      status: 'Online'
+      menuClass: "header-menu",
+      status: "Offline"
     };
   }
 
-  // handler for the responsive menu dropdown
   dropdown () {
-    if (this.state.menuClass === 'header-menu') {this.setState({ menuClass: 'header-menu responsive' });} else {
-      this.setState({ menuClass: 'header-menu' });
+    if (this.state.menuClass === "header-menu") {this.setState({ menuClass: "header-menu responsive" });} else {
+      this.setState({ menuClass: "header-menu" });
     }
   }
 
   render () {
     return (
-      <div>
-        <ShiftDown>
-          <div className="header">
-            <div className="stretcher">
-              <div className="navitem logo">
-                <Link to="/" className="navitem logo-link">
-                  <img
-                    className="navitem logo-diamond"
-                    src="https://vignette.wikia.nocookie.net/spartaremix/images/e/ec/Discord-new-logo.png/revision/latest?cb=20180224071554"
-                  />
-                  <h1 className="navitem logo-name">DiscordCrawler</h1>
-                </Link>
-                <div className="navitem status">
-                  <div
-                    className={
-                      this.state.status === 'Online' ?
-                        'status-green' :
-                        'status-red'
-                    }
-                  />{' '}
-                  {this.state.status}
-                </div>
-              </div>
-              <div className={this.state.menuClass}>
-                <div className="navitem">
-                  <Link to="/">Home</Link>
-                  <Link to="/stats">Stats</Link>
-                  <Link to="/about">About</Link>
-                </div>
-              </div>
-              <i className="navitem fa fa-bars icon" onClick={this.dropdown} />
-            </div>
+      <div className="header">
+        <div className="logo">
+          <Link to="/" class="logo-link">
+            <img className="logo-dcrawl" src="https://seeklogo.com/images/D/discord-logo-134E148657-seeklogo.com.png" />
+            <h1 className="logo-name">DiscordCrawler</h1>
+          </Link>
+          <div className="status">
+            <div className={this.state.status === "Online" ? "status-green" : "status-red"} />
+            {this.state.status}
           </div>
-        </ShiftDown>
+        </div>
+        <div className={this.state.menuClass}>
+          <Link to="/">Live Mode</Link>
+          <Link to="/all">All Servers</Link>
+          <Link to="/stats">Stats</Link>
+        </div>
+        <i className="fa fa-bars icon" onClick={this.dropdown} />
       </div>
     );
   }
 }
 
-export default Navbar;
+export default Header;
