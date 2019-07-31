6773884e147922ba0acf34f1beb7c0ce949412bf
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2019-07-30 19:25:21 -0500

    fixed avatar not rendering correct size

--- frontend/src/components/ServerCard.js
+++ frontend/src/components/ServerCard.js
@@ -7,23 +7,15 @@ import Zoom from 'react-reveal/Zoom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
-const Animation = styled.div.attrs(props => ({
-  style: { animationDelay: `${props.delay}s` } }))`
-  opacity: 0;animation:fadeIn ease-in 1 forwards;animation-fill-mode:
-  forwards;animation-duration:1s;`;
-
 const CardBackground = styled.div.attrs(props => ({
-  style: { background: 'url(' + `${props.bgImage}%` + ')' }
+  style: { background: 'url(' + `${props.bgImage}` + ')' }
 }))``;
 
-const CardAvatar = styled.div.attrs(props => ({
-  style: { background: 'url(' + `${props.avatar}%` + ')' }
-}))``;
-
-const CardJoin = styled.div`    float: right;
-margin-top: 4em;
-margin-right: 1em;
-position: relative;`;
+const CardJoin = styled.div`
+  float: right;
+  margin-top: 4em;
+  margin-right: 1em;
+  position: relative;`;
 
 
 function ServerCard (props) {
@@ -31,10 +23,16 @@ function ServerCard (props) {
   const [cardNumber, setCardNumber] = useState(props.cardNumber);
   const [inviteCode, setInviteCode] = useState(props.code);
 
+  function getJoinURL () {
+    return (
+      'https://discord.gg/' + inviteCode
+    );
+  }
+
   function getIconURL (size = 128) {
-    if (cardData && cardData.guild) {
-      var guildID = cardData.guild.id;
-      var iconID = cardData.guild.icon;
+    if (cardData) {
+      var guildID = cardData['guild_id'];
+      var iconID = cardData['guild_icon'];
       if (iconID === 'undefined.png') {
         return null;
       }
@@ -51,21 +49,17 @@ function ServerCard (props) {
   }
 
   function renderCard () {
-    if (cardData && cardData.guild) {
-      var cardIcon = getIconURL(256);
-      console.log('Rendering card ' + cardNumber);
+    if (cardData) {
+      var cardIcon = getIconURL(128);
+      //console.log('Rendering card ' + cardNumber);
       return (
-        <Zoom duration = "750">
+        <Zoom duration = {300}>
           <div className="CardTop">
             <CardBackground className="Background" bgImage={cardIcon} />
-            <CardAvatar className="Avatar" avatar={cardIcon} />
-            <CardJoin className="CardButton">
-              <div className="CardButtonText">
-                <FontAwesomeIcon icon={faSignInAlt} />&nbsp;&nbsp;Join Now
-              </div>
-            </CardJoin>
+            <div className="Avatar" style={{ background: 'url(' + cardIcon + ')', backgroundSize: '100%' }} />
+
             <div className="info">
-              <h3>{cardData.guild.name}</h3>
+              <h3>{cardData['guild_name']}</h3>
             </div>
           </div>
           <div className="main__content">
@@ -73,11 +67,11 @@ function ServerCard (props) {
               <div className="info2">
                 <div className="pill">
                   <i className="fa fa-circle online" aria-hidden="true" />
-                  {cardData.approximate_presence_count} online<div className="spacer"/>
+                  { cardData ['approximate_presence_count'] } online <div className="spacer"/>
                 </div><div className="spacer"/>
                 <div className="pill">
                   <i className="fa fa-circle" aria-hidden="true" />
-                  {cardData.approximate_member_count} members<div className="spacer"/>
+                  { cardData ['approximate_member_count'] } members <div className="spacer"/>
                 </div><div className="spacer"/>
               </div>
             </div>
@@ -86,21 +80,7 @@ function ServerCard (props) {
       );
     }
   }
-
-  // called each time a parent renders this component to a client DOM
-
   return <div>{renderCard()}</div>;
 }
-
-/*
-ServerCard.defaltProps = {
-    bgimage: '',
-    avatar: '',
-    title: 'default title',
-    subtitle: 'default subtitle',
-    online: '0',
-    total: '0'
-  };
-*/
 
 export default ServerCard;
